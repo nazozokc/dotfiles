1af4c516a7613d19a318277394abc4fb6f240b49
nazozokc <nazozokc@github.com>
2026-01-24 23:19:38 +0900

    new file git-repo.fish

---
+++ fish/functions/git-repo.fish
@@ -0,0 +1,13 @@
+function git-repo
+    set repo (ghq list | fzf \
+        --preview '
+            if test -f (ghq root)/{}/README.md
+                bat --color=always (ghq root)/{}/README.md
+            else
+                ls -lah (ghq root)/{}
+            end
+        ')
+    if test -n "$repo"
+        cd (ghq root)/$repo
+    end
+end
