6ddda031fef716f867fcb7a18266cc152b9fd29b
nazozokc <nazozokc@github.com>
2026-01-24 22:37:32 +0900

    edit wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -54,14 +54,23 @@ config.harfbuzz_features = { "calt", "clig", "liga" }
 -- ======================
 config.color_scheme = "Kanagawa (Gogh)"
 config.colors = {
-  background = "#0a0a0a",
-  cursor_bg = "#c8c093",
-  cursor_fg = "#0a0a0a",
-  cursor_border = "#c8c093",
-  selection_bg = "#2a2a2a",
-  selection_fg = "#ffffff",
-}
+  foreground = "#c5c9c5",
+  background = "#181616",
 
+  cursor_bg = "#C8C093",
+  cursor_fg = "#2D4F67",
+  cursor_border = "#C8C093",
+
+  selection_fg = "#C8C093",
+  selection_bg = "#2D4F67",
+
+  scrollbar_thumb = "#16161D",
+  split = "#16161D",
+
+  ansi = { "#0D0C0C", "#C4746E", "#8A9A7B", "#C4B28A", "#8BA4B0", "#A292A3", "#8EA4A2", "#C8C093" },
+  brights = { "#A6A69C", "#E46876", "#87A987", "#E6C384", "#7FB4CA", "#938AA9", "#7AA89F", "#C5C9C5" },
+  indexed = { [16] = "#B6927B", [17] = "#B98D7B" },
+}
 -- ======================
 -- 透過・見た目
 -- ======================
