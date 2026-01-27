f1c90d899ddb2eab5c7b19d488dd5806f770a256
nazozokc <nazozokc@gmail.com>
2026-01-27 20:48:28 +0900

    edit neovim-plugins

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -10,9 +10,16 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     lazy = false,
-    opts = {
-      auto_install = true,
-    },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "ts_ls",
+          "lua_ls",
+          "html",
+          "solargraph",
+        },
+      })
+    end,
   },
 
   {
@@ -22,8 +29,42 @@ return {
       local capabilities =
           require("cmp_nvim_lsp").default_capabilities()
 
+      -- ts_ls（TypeScript / 軽量化）
       vim.lsp.config("ts_ls", {
         capabilities = capabilities,
+        single_file_support = false,
+
+        init_options = {
+          hostInfo = "neovim",
+          watchOptions = {
+            ignoredFiles = {
+              "**/node_modules/**",
+              "**/.git/**",
+              "**/dist/**",
+              "**/build/**",
+            },
+          },
+        },
+
+        settings = {
+          typescript = {
+            inlayHints = {
+              includeInlayParameterNameHints = "none",
+              includeInlayFunctionParameterTypeHints = false,
+              includeInlayVariableTypeHints = false,
+              includeInlayFunctionReturnTypeHints = false,
+            },
+          },
+          javascript = {
+            inlayHints = {
+              includeInlayParameterNameHints = "none",
+            },
+          },
+        },
+
+        on_attach = function(client)
+          client.server_capabilities.documentFormattingProvider = false
+        end,
       })
 
       vim.lsp.config("solargraph", {
@@ -46,10 +87,15 @@ return {
       })
 
       -- LSP keymaps
-      vim.keymap.set("n", "K", vim.lsp.buf.hover)
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition)
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references)
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(ev)
+          local opts = { buffer = ev.buf }
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
+          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+        end,
+      })
     end,
   },
 }
