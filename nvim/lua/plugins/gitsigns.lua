69288478e0e073ab149c92bf6ef4b35670b2c716
nazozokc <nazozokc@gmail.com>
2026-02-04 19:25:43 +0900

    edit plugins

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,12 +1,22 @@
 return {
-  {
-    "tpope/vim-fugitive",
+  "lewis6991/gitsigns.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    signs = {
+      add          = { text = "▎" },
+      change       = { text = "▎" },
+      delete       = { text = "" },
+      topdelete    = { text = "" },
+      changedelete = { text = "▎" },
+    },
+
+    signcolumn = true, -- 左端に常時表示
+    watch_gitdir = {
+      follow_files = true,
+    },
+
+    current_line_blame = false, -- ← 後で有効にする
+    word_diff = true,           -- 🔥 単語単位差分
+    update_debounce = 100,
   },
-  {
-    "lewis6991/gitsigns.nvim",
-    event = { "BufReadPost" },
-    config = function()
-      require("gitsigns").setup()
-    end,
-  }
 }
