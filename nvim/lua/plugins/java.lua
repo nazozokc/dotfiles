b54192fdcdd91ce2f1ca4f963d7ca8b2e406dc31
nazozokc <nazozokc@gmail.com>
2026-02-05 19:28:55 +0900

    add java.lua

---
+++ nvim/lua/plugins/java.lua
@@ -0,0 +1,21 @@
+return {
+  "nvim-java/nvim-java",
+  dependencies = {
+    "neovim/nvim-lspconfig",
+    "mfussenegger/nvim-dap", -- Debug プラグイン
+    "MunifTanjim/nui.nvim",  -- UI 補助
+  },
+  ft = { "java" },
+  config = function()
+    require("java").setup({
+      jdtls = { version = "latest" },         -- jdtls のバージョン
+      lombok = { enable = true },             -- Lombok 対応
+      java_test = { enable = true },          -- テスト
+      java_debug_adapter = { enable = true }, -- デバッグ
+      spring_boot_tools = { enable = true },  -- Spring Boot
+      jdk = { auto_install = true, version = "17" },
+    })
+    -- LSP を有効化
+    require("lspconfig").jdtls.setup({})
+  end,
+}
