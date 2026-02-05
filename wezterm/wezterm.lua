4d13634481f05bdfb7f5bbbab3c32c898669ee2a
nazozokc <nazozokc@gmail.com>
2026-02-05 17:08:16 +0900

    add homepage

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,6 +4,8 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
+config.automatically_reload_config = true
+
 require("config.appearance").apply(config)
 require("config.font").apply(config)
 require("config.keys").apply(config)
