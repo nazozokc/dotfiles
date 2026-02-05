9ced19e449c12340e4205e80c2301ba51c638f0d
nazozokc <nazozokc@gmail.com>
2026-02-05 23:24:46 +0900

    add nvim

--- nvim/lua/plugins/java.lua
+++ nvim/lua/plugins/java.lua
@@ -1,21 +1,28 @@
 return {
-  "nvim-java/nvim-java",
-  dependencies = {
-    "neovim/nvim-lspconfig",
-    "mfussenegger/nvim-dap", -- Debug プラグイン
-    "MunifTanjim/nui.nvim",  -- UI 補助
+  {
+    "nvim-java/nvim-java",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "mfussenegger/nvim-dap", -- Debug プラグイン
+      "MunifTanjim/nui.nvim",  -- UI 補助
+    },
+    ft = { "java" },
+    config = function()
+      require("java").setup({
+        jdtls = { version = "latest" },         -- jdtls のバージョン
+        lombok = { enable = true },             -- Lombok 対応
+        java_test = { enable = true },          -- テスト
+        java_debug_adapter = { enable = true }, -- デバッグ
+        spring_boot_tools = { enable = true },  -- Spring Boot
+        jdk = { auto_install = true, version = "17" },
+      })
+      -- LSP を有効化
+      require("lspconfig").jdtls.setup({})
+    end,
   },
-  ft = { "java" },
-  config = function()
-    require("java").setup({
-      jdtls = { version = "latest" },         -- jdtls のバージョン
-      lombok = { enable = true },             -- Lombok 対応
-      java_test = { enable = true },          -- テスト
-      java_debug_adapter = { enable = true }, -- デバッグ
-      spring_boot_tools = { enable = true },  -- Spring Boot
-      jdk = { auto_install = true, version = "17" },
-    })
-    -- LSP を有効化
-    require("lspconfig").jdtls.setup({})
-  end,
+  {
+    "some/spring-boot.nvim",
+    ft = { "java" },
+    requires = { "nvim-java/nvim-java" },
+  },
 }
