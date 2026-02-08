2c9b5a455d5cb6f5b5474d634d21070f01f89ca0
nazozokc <nazozokc@gmail.com>
2026-02-08 17:04:44 +0900

    edit nix

--- nix/packages.nix
+++ nix/packages.nix
@@ -1,12 +1,13 @@
 { pkgs }:
 
 with pkgs; [
+# terminal
   neovim
   wezterm
   ghostty
-
   fish
 
+# 文字入力
   fcitx5
   fcitx5-mozc
   qt6Packages.fcitx5-configtool
@@ -14,6 +15,8 @@ with pkgs; [
   spotify
   google-chrome
 
+# ソフトインストール,github-cli
+gh
   git
   curl
   wget
