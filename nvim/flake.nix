22b13db141311c6853d5882ea0e6a963a89dc9b3
nazozokc <nazozokc@gmail.com>
2026-02-08 14:41:26 +0900

    d

--- nvim/flake.nix
+++ nvim/flake.nix
@@ -1,9 +1,44 @@
-packages = with pkgs; [
-  nodejs_20
-  nodePackages.typescript
-  nodePackages.typescript-language-server
-  nodePackages.eslint
-  lua-language-server
-  html-languageserver
-  solargraph
-];
+{
+  description = "Neovim LSP devShell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          # Node / Web
+          nodejs_20
+          nodePackages.typescript
+          nodePackages.typescript-language-server
+          nodePackages.eslint
+          vscode-langservers-extracted
+
+          # Lua
+          lua-language-server
+
+          # Ruby
+          solargraph
+
+          # Rust
+          rust-analyzer
+          rustc
+          cargo
+
+          # C / C++
+          clang
+          clang-tools
+
+          # Java
+          jdk21
+          jdt-language-server
+        ];
+      };
+    };
+}
+
