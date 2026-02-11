288d5aa4e0befb9a9987db6199ce7db53c513a7c
nazozokc <nazozokc@gmail.com>
2026-02-11 20:25:49 +0900

    edit nix/

--- flake.nix
+++ flake.nix
@@ -10,32 +10,38 @@
     };
 
     zen-browser = {
-    url = "github:youwen5/zen-browser-flake";
-    inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:youwen5/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { nixpkgs, home-manager, zen-browser, ... }:
   let
-    system = "x86_64-linux";
+    username = "nazozokc";
 
-    # nixpkgs を allowUnfree 許可で読み込む
-    pkgs = import nixpkgs {
-      inherit system;
-      config = {
-        allowUnfree = true;   # ← ここで unfree パッケージ許可
-      };
-    };
-  in {
-    homeConfigurations.nazozokc =
+    mkHome = system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in
       home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
+        extraSpecialArgs = {
+          inherit zen-browser;
+        };
+
         modules = [
-          ({ config, pkgs, ... }: {
-          username = nazozokc;
-            home.username = ${username};
-            home.homeDirectory = "/home/${username}";
+          {
+            home.username = username;
+
+            home.homeDirectory =
+              if pkgs.stdenv.isDarwin
+              then "/Users/${username}"
+              else "/home/${username}";
+
             home.stateVersion = "24.05";
 
             programs.home-manager.enable = true;
@@ -46,8 +52,18 @@
               ./nix/pkgs/nvim.nix
               ./nix/pkgs/gui/gui-tool.nix
             ];
-          })
+          }
         ];
       };
+  in
+  {
+    homeConfigurations = {
+      # 今使ってるLinux
+      ${username} = mkHome "x86_64-linux";
+
+      # 将来Macに行ったら追加するだけ
+      # ${username}-mac = mkHome "aarch64-darwin";
+    };
   };
 }
+
