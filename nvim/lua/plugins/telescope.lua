09677579390ce377eb9645fbef13e3bfe090ffc2
nazozokc <nazozokc@gmail.com>
2026-02-11 11:16:42 +0900

    edit lif

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -6,9 +6,9 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     event = "VeryLazy",
-tag = "0.1.5",
+    tag = "0.1.5",
     dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
+    config = function()
       require("telescope").setup({
         extensions = {
           ["ui-select"] = {
