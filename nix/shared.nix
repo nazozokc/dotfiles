59e326e9c988d8729c4a69b26388533a2378afc5
nazozokc <nazozokc@gmail.com>
2026-02-12 16:43:57 +0900

    edit nix

---
+++ nix/shared.nix
@@ -0,0 +1,27 @@
+{ pkgs, lib, username, ... }:
+
+{
+  ########################################
+  # 基本情報（共通）
+  ########################################
+  home.username = username;
+
+  home.homeDirectory =
+    if pkgs.stdenv.isDarwin
+    then "/Users/${username}"
+    else "/home/${username}";
+
+  # Home Manager の互換性バージョン
+  home.stateVersion = "24.05";
+
+  ########################################
+  # XDG 有効化（.config 管理用）
+  ########################################
+  xdg.enable = true;
+
+  ########################################
+  # Home Manager 自身を有効化
+  ########################################
+  programs.home-manager.enable = true;
+}
+
