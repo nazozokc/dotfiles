c48275ebec324bff8abfd35bbb5758baa5b80e73
nazozokc <nazozokc@gmail.com>
2026-02-12 19:03:03 +0900

    edit nix

--- nix/home-manager/common.nix
+++ nix/home-manager/common.nix
@@ -6,31 +6,17 @@ let
   dotfilesDir = "${homeDir}/ghq/github.com/nazozokc/dotfiles";
 
   ########################################
-  # パッケージまとめ
+  # 各 myPkgs をフラット化
   ########################################
-  cliPkgs = with myPkgs.pkgs; [
-    myPkgs.cli.core
-    myPkgs.cli.git
-    myPkgs.cli.misc
-    myPkgs.cli.search
-  ];
-
-  guiPkgs = with myPkgs.pkgs; [
-    myPkgs.gui.editor
-    myPkgs.gui.terminal
-    myPkgs.gui.misc
-  ];
-
-  langPkgs = with myPkgs.pkgs; [
-    myPkgs.lang.node
-    myPkgs.lang.python
-    myPkgs.lang.rust
-    myPkgs.tools
-  ];
-
+  cliPkgs = lib.concatMap (x: x) myPkgs.cli;
+  guiPkgs = lib.concatMap (x: x) myPkgs.gui;
+  langPkgs = lib.concatMap (x: x) myPkgs.lang;
 in
 {
-  home.packages = cliPkgs ++ guiPkgs ++ langPkgs;
+  ########################################
+  # インストールするパッケージ
+  ########################################
+  home.packages = cliPkgs ++ guiPkgs ++ langPkgs ++ myPkgs.tools;
 
   ########################################
   # dotfilesリンク
@@ -44,17 +30,14 @@ in
   home.activation.linkDotfilesCommon =
     lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       echo "linking extra dotfiles (force)"
-      ln -sfn "${dotfilesDir}/pip" "${homeDir}/.pip"
+      ln -sfn "${dotfilesDir}/pip"        "${homeDir}/.pip"
       ln -sfn "${dotfilesDir}/my_scripts" "${homeDir}/.scripts"
     '';
 
   ########################################
   # プログラム有効化
   ########################################
-  programs.fish.enable    = true;
-  programs.neovim.enable  = true;
-  programs.wezterm.enable = true;
-  xdg.enable              = true;
+  xdg.enable               = true;
   programs.home-manager.enable = true;
 }
 
