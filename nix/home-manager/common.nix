c1ed2f7675e8716e627655bd6e5f94c356a64329
nazozokc <nazozokc@gmail.com>
2026-02-12 19:26:53 +0900

    edit nix

--- nix/home-manager/common.nix
+++ nix/home-manager/common.nix
@@ -1,28 +1,28 @@
-{ lib, config, myPkgs, ... }:
+{ lib, pkgs, config, ... }:
+
+with pkgs;
 
 let
+  # パッケージ
+  cliPkgs  = import ../pkgs/cli/default.nix  { inherit pkgs; };
+  guiPkgs  = import ../pkgs/gui/default.nix  { inherit pkgs; };
+  langPkgs = import ../pkgs/lang/default.nix { inherit pkgs; };
+
   homeDir    = config.home.homeDirectory;
   dotfilesDir = "${homeDir}/ghq/github.com/nazozokc/dotfiles";
-
 in
 {
-  ########################################
-  # dotfilesリンクだけ作る
-  ########################################
-  home.activation.linkDotfilesCommon =
-    lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      echo "linking extra dotfiles (force)"
-      ln -sfn "${dotfilesDir}/fish"        "${homeDir}/.config/fish"
-      ln -sfn "${dotfilesDir}/nvim"        "${homeDir}/.config/nvim"
-      ln -sfn "${dotfilesDir}/wezterm"     "${homeDir}/.config/wezterm"
-      ln -sfn "${dotfilesDir}/pip"         "${homeDir}/.pip"
-      ln -sfn "${dotfilesDir}/my_scripts"  "${homeDir}/.scripts"
-    '';
+  # パッケージのインストール
+  home.packages = cliPkgs ++ guiPkgs ++ langPkgs;
 
-  ########################################
-  # xdg と home-manager は有効にしておく
-  ########################################
-  xdg.enable               = true;
-  programs.home-manager.enable = true;
+  # dotfiles シンボリックリンク
+  home.activation.linkDotfiles = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    echo "Linking extra dotfiles..."
+    ln -sfn "${dotfilesDir}/fish"        "${homeDir}/.config/fish"
+    ln -sfn "${dotfilesDir}/nvim"        "${homeDir}/.config/nvim"
+    ln -sfn "${dotfilesDir}/wezterm"     "${homeDir}/.config/wezterm"
+    ln -sfn "${dotfilesDir}/pip"         "${homeDir}/.pip"
+    ln -sfn "${dotfilesDir}/my_scripts"  "${homeDir}/.scripts"
+  '';
 }
 
