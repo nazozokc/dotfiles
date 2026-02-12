e1cee0ee76b23c80c5e976869794270cbe3650ae
nazozokc <nazozokc@gmail.com>
2026-02-12 23:10:30 +0900

    edit nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "nazozo dotfiles (Linux/macOS unified)";
+  description = "nazozo dotfiles (my home-manager & nix-darwin)";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -15,20 +15,15 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, darwin, lib, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, darwin, ... }@inputs:
   let
     username = "nazozokc";
 
-    # システムごとの pkgs
+    # システムごとの pkgs を生成
     pkgsFor = system: import nixpkgs {
       inherit system;
       config.allowUnfree = true;
     };
-
-    # 共通モジュール呼び出し
-    linuxPkgs = pkgsFor "x86_64-linux";
-    darwinPkgs = pkgsFor "aarch64-darwin";
-
   in
   {
     ########################################
@@ -36,7 +31,7 @@
     ########################################
     homeConfigurations.${username} =
       home-manager.lib.homeManagerConfiguration {
-        pkgs = linuxPkgs;
+        pkgs = pkgsFor "x86_64-linux";
 
         extraSpecialArgs = { inherit username inputs; };
 
@@ -46,8 +41,15 @@
           ./nix/home-manager/linux.nix
         ];
 
-        home.packages = import ./nix/home-manager/common.nix { pkgs = linuxPkgs; };
-        home.activation = import ./nix/home-manager/symlinks.nix { pkgs = linuxPkgs; inherit username; };
+        # パッケージまとめ
+        home.packages = import ./nix/home-manager/common.nix { pkgs = pkgsFor "x86_64-linux"; };
+
+        # symlink 作成
+        home.activation = import ./nix/home-manager/symlinks.nix { 
+          pkgs = pkgsFor "x86_64-linux"; 
+          lib  = pkgsFor "x86_64-linux".lib;
+          inherit username; 
+        };
       };
 
     ########################################
@@ -56,61 +58,64 @@
     darwinConfigurations.${username} =
       darwin.lib.darwinSystem {
         system = "aarch64-darwin";
+
         specialArgs = { inherit username inputs; };
 
         modules = [
           ./nix/os/darwin.nix
         ];
 
-        packages = import ./nix/home-manager/common.nix { pkgs = darwinPkgs; };
-        activation = import ./nix/home-manager/symlinks.nix { pkgs = darwinPkgs; inherit username; };
-      };
+        # パッケージまとめ
+        packages = import ./nix/home-manager/common.nix { pkgs = pkgsFor "aarch64-darwin"; };
 
+        # symlink 作成
+        activation = import ./nix/home-manager/symlinks.nix { 
+          pkgs = pkgsFor "aarch64-darwin"; 
+          lib  = pkgsFor "aarch64-darwin".lib;
+          inherit username; 
+        };
+      };
+    
     ########################################
-    # Apps / スクリプト化
+    # apps スクリプト化（Linux/macOS 両対応）
     ########################################
-    apps = lib.genAttrs [ "x86_64-linux" "aarch64-darwin" ] (system: let
-      pkgs = if system == "x86_64-linux" then linuxPkgs else darwinPkgs;
-      homeDir = if system == "x86_64-linux" then "/home/${username}" else "/Users/${username}";
-    in {
+    apps = let
+      linuxPkgs   = pkgsFor "x86_64-linux";
+      darwinPkgs  = pkgsFor "aarch64-darwin";
+    in
+    {
       switch = {
         type = "app";
         program = ''
-          set -eo pipefail
-          SYSTEM="$(uname)"
-          echo "Switching configuration..."
-          if [ "$SYSTEM" = "Darwin" ]; then
-            sudo nix run nix-darwin -- switch --flake .#${username}
+          if [ "$(uname)" = "Darwin" ]; then
+            nix run nixpkgs#nix-darwin -- switch --flake .#${username}
           else
             nix run nixpkgs#home-manager -- switch --flake .#${username}
           fi
-          echo "Done!"
-        '';
-      };
-
-      update = {
-        type = "app";
-        program = ''
-          set -e
-          echo "Updating flake.lock..."
-          nix flake update
-          echo "Done! Run 'nix run .#switch' to apply changes."
         '';
       };
 
       update-node-packages = {
         type = "app";
         program = ''
-          set -e
-          DOTFILES_DIR="${homeDir}/ghq/github.com/nazozokc/dotfiles"
-          if [ ! -d "$DOTFILES_DIR" ]; then
-            DOTFILES_DIR="$(pwd)"
+          echo "Updating Node.js packages..."
+          if [ "$(uname)" = "Darwin" ]; then
+            DOTFILES_DIR="/Users/${username}/ghq/github.com/nazozokc/dotfiles"
+          else
+            DOTFILES_DIR="/home/${username}/ghq/github.com/nazozokc/dotfiles"
           fi
-          ${pkgs.bash}/bin/bash "$DOTFILES_DIR/nix/packages/node/update.sh" "$@"
-          echo "Node packages updated!"
+          bash "$DOTFILES_DIR/nix/packages/node/update.sh"
         '';
       };
-    });
+
+      update-flake = {
+        type = "app";
+        program = ''
+          echo "Updating flake inputs..."
+          nix flake update
+        '';
+      };
+    };
   };
 }
 
