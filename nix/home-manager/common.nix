65baaecbe96c1b3e60f10a30b1bc2b9213c9f7d9
nazozokc <nazozokc@gmail.com>
2026-02-12 19:09:27 +0900

    edit nix

--- nix/home-manager/common.nix
+++ nix/home-manager/common.nix
@@ -1,43 +1,30 @@
 { lib, config, myPkgs, ... }:
 
 let
-  homeDir   = config.home.homeDirectory;
-  configHome = config.xdg.configHome;
+  homeDir    = config.home.homeDirectory;
   dotfilesDir = "${homeDir}/ghq/github.com/nazozokc/dotfiles";
-
-  ########################################
-  # 各 myPkgs をフラット化
-  ########################################
-  cliPkgs = lib.concatMap (x: x) myPkgs.cli;
-  guiPkgs = lib.concatMap (x: x) myPkgs.gui;
-  langPkgs = lib.concatMap (x: x) myPkgs.lang;
 in
 {
-  ########################################
-  # インストールするパッケージ
-  ########################################
-  home.packages = lib.concatMap (x: x) (myPkgs.cli ++ myPkgs.gui ++ myPkgs.lang ++ myPkgs.tools);
-
   ########################################
   # dotfilesリンク
   ########################################
   xdg.configFile = {
-    "fish"   = { source = "${dotfilesDir}/fish"; force = true; };
-    "nvim"   = { source = "${dotfilesDir}/nvim"; force = true; };
-    "wezterm"= { source = "${dotfilesDir}/wezterm"; force = true; };
+    "fish"    = { source = "${dotfilesDir}/fish"; force = true; };
+    "nvim"    = { source = "${dotfilesDir}/nvim"; force = true; };
+    "wezterm" = { source = "${dotfilesDir}/wezterm"; force = true; };
   };
 
   home.activation.linkDotfilesCommon =
     lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       echo "linking extra dotfiles (force)"
-      ln -sfn "${dotfilesDir}/pip"        "${homeDir}/.pip"
+      ln -sfn "${dotfilesDir}/pip"         "${homeDir}/.pip"
       ln -sfn "${dotfilesDir}/my_scripts" "${homeDir}/.scripts"
     '';
 
   ########################################
   # プログラム有効化
   ########################################
-  xdg.enable               = true;
+  xdg.enable                   = true;
   programs.home-manager.enable = true;
 }
 
