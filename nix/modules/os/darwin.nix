da6273177b25fb2a210825b76de78a46dc19d437
nazozokc <nazozokc@gmail.com>
2026-02-12 16:04:18 +0900

    edit nix

--- nix/modules/os/darwin.nix
+++ nix/modules/os/darwin.nix
@@ -1,13 +1,38 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
+  ########################################
+  # macOS 専用 Home Manager 設定
+  ########################################
+
+  # macOS でだけ入れたいパッケージ
   home.packages = with pkgs; [
-    terminal-notifier
+    coreutils
+    gnugrep
+    gnutar
   ];
 
+  ########################################
+  # macOS 向け PATH / 環境変数
+  ########################################
   home.sessionVariables = {
-    EDITOR = "nvim";
-    BROWSER = "zen-browser";
+    LANG = "en_US.UTF-8";
   };
+
+  ########################################
+  # macOS 専用プログラム設定
+  ########################################
+  programs.zsh.enable = lib.mkDefault false;
+  programs.fish.enable = lib.mkDefault true;
+
+  ########################################
+  # Darwin 判定ガード（安全装置）
+  ########################################
+  assertions = [
+    {
+      assertion = pkgs.stdenv.isDarwin;
+      message = "os/darwin.nix is imported on a non-darwin system";
+    }
+  ];
 }
 
--- nix/modules/os/linux.nix
+++ nix/modules/os/linux.nix
@@ -1,18 +1,36 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
+  ########################################
+  # Linux 専用 Home Manager 設定
+  ########################################
+
+  # Linux でだけ入れたいパッケージ
   home.packages = with pkgs; [
     xclip
     wl-clipboard
-    xdg-utils
   ];
 
-  xdg.enable = true;
-  fonts.fontconfig.enable = true;
-
+  ########################################
+  # Linux 向け環境変数
+  ########################################
   home.sessionVariables = {
-    EDITOR = "nvim";
-    BROWSER = "zen-browser";
+    LANG = "en_US.UTF-8";
   };
+
+  ########################################
+  # Linux 専用プログラム
+  ########################################
+  programs.fish.enable = lib.mkDefault true;
+
+  ########################################
+  # Linux 判定ガード
+  ########################################
+  assertions = [
+    {
+      assertion = pkgs.stdenv.isLinux;
+      message = "os/linux.nix is imported on a non-linux system";
+    }
+  ];
 }
 
