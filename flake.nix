690ae98987434d0f1b0deaeefcb56802a3db9bdb
nazozokc <nazozokc@gmail.com>
2026-02-13 09:36:30 +0900

    edit nix

--- flake.nix
+++ flake.nix
@@ -72,20 +72,20 @@
   in {
     switch = {
       type = "app";
-      program = linuxPkgs.writeShellScriptBin "switch" ''
+      program = builtins.toString (linuxPkgs.writeShellScriptBin "switch" ''
         echo "Building and switching Linux Home Manager config..."
         nix run nixpkgs#home-manager -- switch --flake .#${username}
         echo "Done!"
-      '';
+      '');
     };
 
     update-node-packages = {
       type = "app";
-      program = linuxPkgs.writeShellScriptBin "update-node-packages" ''
+      program = builtins.toString (linuxPkgs.writeShellScriptBin "update-node-packages" ''
         echo "Updating Node packages..."
         ${linuxPkgs.bash}/bin/bash ./nix/packages/node/update.sh
         echo "Done!"
-      '';
+      '');
     };
   };
 
@@ -94,20 +94,20 @@
   in {
     switch = {
       type = "app";
-      program = darwinPkgs.writeShellScriptBin "switch" ''
+      program = builtins.toString (darwinPkgs.writeShellScriptBin "switch" ''
         echo "Building and switching macOS nix-darwin config..."
         sudo nix run nix-darwin -- switch --flake .#${username}
         echo "Done!"
-      '';
+      '');
     };
 
     update-node-packages = {
       type = "app";
-      program = darwinPkgs.writeShellScriptBin "update-node-packages" ''
+      program = builtins.toString (darwinPkgs.writeShellScriptBin "update-node-packages" ''
         echo "Updating Node packages on macOS..."
         ${darwinPkgs.bash}/bin/bash ./nix/packages/node/update.sh
         echo "Done!"
-      '';
+      '');
     };
   };
 };
