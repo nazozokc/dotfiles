85ab2e4786868098a585dbfd6868148386cc70b2
nazozokc <nazozokc@gmail.com>
2026-02-14 21:38:21 +0900

    edit nix

--- nix/modules/home-manager/common.nix
+++ nix/modules/home-manager/common.nix
@@ -6,7 +6,7 @@
       cliPkgs = import ../defaults/cli-default.nix { inherit pkgs; };
       guiPkgs = import ../defaults/gui-default.nix { inherit pkgs; };
       langPkgs = import ../defaults/lang-default.nix { inherit pkgs; };
-      nodePkgs = import ../defaults/node-pkgs.nix { inherit pkgs;}
+      nodePkgs = import ../defaults/node-pkgs.nix { inherit pkgs; };
     in
     cliPkgs ++ guiPkgs ++ langPkgs ++ nodePkgs;
 }
