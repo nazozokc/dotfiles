cb3cf5356f1051b04b40142c466a6553a357d205
nazozokc <nazozokc@gmail.com>
2026-02-14 20:49:34 +0900

    edit nix

--- nix/packages/node/default.nix
+++ nix/packages/node/default.nix
@@ -32,8 +32,8 @@ in
   npm = mkNpmPackage {
     pname        = "npm";
     version      = "11.10.0";
-    hash         = "sha256-AAA";
-    npmDepsHash  = "sha256-AAA";
+    hash         = "088ms8vawca58qxwlzfa4jf1a2a82j49dysijq3v3h4fq394mc59";
+    npmDepsHash  = "";
     description  = "Node package manager";
     homepage     = "https://www.npmjs.com/";
     mainProgram  = "npm";
@@ -42,8 +42,8 @@ in
   npx = mkNpmPackage {
     pname        = "npx";
     version      = "10.2.2";
-    hash         = "sha256-AAA";
-    npmDepsHash  = "sha256-AAA";
+    hash         = "08y76vn8b1l60mjzg2rlr9h6hcaybdd8xrpx240m1x0zwrd47xwf";
+    npmDepsHash  = "";
     description  = "npm package runner";
     homepage     = "https://www.npmjs.com/package/npx";
     mainProgram  = "npx";
@@ -52,8 +52,8 @@ in
   pnpm = mkNpmPackage {
     pname        = "pnpm";
     version      = "10.29.3";
-    hash         = "sha256-AAA";
-    npmDepsHash  = "sha256-AAA";
+    hash         = "13gfcxy9y3sa7xrql4p0dy57iiryfji1hkz2cmkx4vmjmnrv36lx";
+    npmDepsHash  = "";
     description  = "Fast, disk space efficient package manager";
     homepage     = "https://pnpm.io/";
   };
