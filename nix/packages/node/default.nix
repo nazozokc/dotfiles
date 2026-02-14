eaed190fe7506cb7e7a9eaaf5097a49185c3def0
nazozokc <nazozokc@gmail.com>
2026-02-14 10:21:26 +0900

    edit nix

--- nix/packages/node/default.nix
+++ nix/packages/node/default.nix
@@ -1,20 +1,43 @@
-{ pkgs }:
+{ pkgs, lib ? pkgs.lib }:
 
 let
-  node = pkgs.nodejs_20;
+  inherit (pkgs) buildNpmPackage fetchzip;
 
-  nodePkgs = pkgs.nodePackages;
+  # npm CLI パッケージを Nix で管理するためのヘルパー
+  mkNpmPackage = { pname, npmName ? pname, version, hash, npmDepsHash, description, homepage, license ? lib.licenses.mit, mainProgram ? pname }: 
+    buildNpmPackage rec {
+      inherit pname version npmDepsHash;
+      src = fetchzip {
+        url = "https://registry.npmjs.org/${npmName}/-/${pname}-${version}.tgz";
+        inherit hash;
+      };
+      dontNpmBuild = true;
 
+      meta = {
+        inherit description homepage license mainProgram;
+      };
+    };
 in
 {
-  # Node 本体
-  inherit node;
+  # 例：UnoCSS Language Server
+  unocss-language-server = mkNpmPackage {
+    pname = "unocss-language-server";
+    version = "0.1.8";
+    hash = "sha256-jHFuTpcf4dk8i7Ty1HS9A8OOLarct3w/jovE6/KZHDs=";
+    npmDepsHash = "sha256-hqo+J1o4sG+jGQBSGwQ1uvSCS9mCFX6TfjEC9kut9fI=";
+    description = "UnoCSS Language Server";
+    homepage = "https://github.com/unocss/unocss";
+    mainProgram = "unocss-language-server";
+  };
 
-  # よく使うCLI
-  pnpm = nodePkgs.pnpm;
-  typescript = nodePkgs.typescript;
-  eslint = nodePkgs.eslint;
-  prettier = nodePkgs.prettier;
-
-  # 必要ならここに追加していく
+  # 例：Prettier
+  prettier = mkNpmPackage {
+    pname = "prettier";
+    version = "3.5.0";
+    hash = "sha256-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa=";
+    npmDepsHash = "sha256-bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb=";
+    description = "Prettier code formatter";
+    homepage = "https://prettier.io/";
+  };
 }
+
