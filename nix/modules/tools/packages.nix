98983d9f5813a3b8f431bee0d315741793f57a2b
nazozokc <nazozokc@gmail.com>
2026-02-14 22:31:34 +0900

    edit nix

---
+++ nix/modules/tools/packages.nix
@@ -0,0 +1,35 @@
+{ pkgs }:
+
+with pkgs; [
+
+  #Editor
+  neovim
+  vscode
+  zed
+
+  #CLI tools
+  coreutils
+  bun
+  jq
+  curl
+  wget
+  zoxide
+  tree
+  htop
+  file
+  which
+  fzf
+  node2nix
+  
+  #github
+  git
+  gh
+  lazygit
+  git-wt
+
+  #GUI software
+  wezterm
+  spotify
+  discord
+  ghostty
+]
