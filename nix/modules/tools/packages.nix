87858ea1157c280dd190072fd117572ef36c5099
nazozokc <nazozokc@gmail.com>
2026-02-15 12:36:39 +0900

    edit nix

--- nix/modules/tools/packages.nix
+++ nix/modules/tools/packages.nix
@@ -16,6 +16,7 @@ with pkgs; [
   zoxide
   tree
   htop
+  fastfetch
   file
   which
   fzf
@@ -32,6 +33,7 @@ with pkgs; [
   pkgs.nixfmt
   rustc
   rust-analyzer
+  rust
   python312
   
   #github
