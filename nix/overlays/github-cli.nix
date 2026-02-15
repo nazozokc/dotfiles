d7486d36f7d6da35994aac3059b7ebe6f4a27977
nazozokc <nazozokc@gmail.com>
2026-02-15 21:16:03 +0900

    edit nix

--- nix/overlays/github-cli.nix
+++ nix/overlays/github-cli.nix
@@ -1,28 +1,14 @@
-# nix/overlays/github-cli.nix
-#
-# GitHub CLI extensions overlay
-#
-# Usage:
-# overlays = [
-#   (import ./nix/overlays/github-cli.nix { 
-#     inherit gh-graph gh-nippou;
-#   })
-# ];
-
 { gh-graph, gh-nippou }:
 
 final: prev:
 
+let
+  ghGraphPkg  = gh-graph.packages.${final.system}.default;
+  ghNippouPkg = gh-nippou.packages.${final.system}.default;
+in
 {
-  ########################################
-  # GitHub CLI Extensions
-  ########################################
-
-  gh-graph = gh-graph.packages.${final.system}.default;
-
-  gh-nippou = gh-nippou.packages.${final.system}.default;
-
-  # ここに将来追加できる
-  # gh-something = gh-something.packages.${final.system}.default;
+  gh-graph  = ghGraphPkg;
+  gh-nippou = ghNippouPkg;
 }
+
 
