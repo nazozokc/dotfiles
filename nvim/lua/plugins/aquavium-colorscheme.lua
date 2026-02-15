69ae9f9643685be3b562555059fa4d1d44353b17
nazozokc <nazozokc@gmail.com>
2026-02-15 11:49:50 +0900

    edit nix

---
+++ nvim/lua/plugins/aquavium-colorscheme.lua
@@ -0,0 +1,19 @@
+return {
+    "https://github.com/T-b-t-nchos/Aquavium.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        local aquavium = require("Aquavium")
+
+        aquavium.setup({
+            -- options
+
+            ---- For example,
+           bold = false,
+           italic = false,
+           transparent = false,
+        })
+
+        vim.cmd("colorscheme Aquavium")
+    end,
+},
