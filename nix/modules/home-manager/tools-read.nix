bc557d397fd2e3d2466d5ec1dd845e7a6c50406a
nazozokc <nazozokc@gmail.com>
2026-02-15 19:24:29 +0900

    edit nix

--- nix/modules/home-manager/tools-read.nix
+++ nix/modules/home-manager/tools-read.nix
@@ -4,7 +4,6 @@
   home.packages =
     let
       Pkgs = import ../tools/packages.nix { inherit pkgs; };
-      gh = import ../tools/program/gh.nix { inherit pkgs; };
     in
-    Pkgs ++ gh;
+    Pkgs;
 }
