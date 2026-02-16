7dcdc7785424341d4d706517317cc70b32f00858
nazozokc <nazozokc@gmail.com>
2026-02-16 21:26:49 +0900

    edit nix

--- nix/packages/node/default.nix
+++ nix/packages/node/default.nix
@@ -43,25 +43,4 @@ let
       };
     };
 in
-{
-  nodejs = pkgs.nodejs-20_x;
-
-  npm = mkNpmPackage {
-    pname = "npm";
-    version = "11.10.0";
-    description = "Node package manager";
-    hash = "sha256-Q8ZTOEw5YXdWhGrUBXBQYaePtrvdss7VerefuS6K8qc=";
-    npmDepsHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
-    homepage = "https://www.npmjs.com/";
-    mainProgram = "npm";
-  };
-
-  pnpm = mkNpmPackage {
-    pname = "pnpm";
-    version = "10.29.3";
-    description = "Fast, disk-space efficient package manager";
-    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
-    npmDepsHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
-    homepage = "https://pnpm.io/";
-  };
-}
+{}
