8124c112f50738fa94d3c69e7c82cb106b24f4a3
nazozokc <nazozokc@gmail.com>
2026-02-19 22:00:40 +0900

    edit nvim lsp-config

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,82 +1,86 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local capabilities =
-          require("cmp_nvim_lsp").default_capabilities()
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      -- ===== 共通 on_attach =====
-      vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(args)
-          local client = vim.lsp.get_client_by_id(args.data.client_id)
-          local bufnr = args.buf
+			-- ===== 共通 on_attach =====
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(args)
+					local client = vim.lsp.get_client_by_id(args.data.client_id)
+					local bufnr = args.buf
 
-          -- ts_ls の semanticTokens を無効化（重い）
-          if client and client.name == "ts_ls" then
-            client.server_capabilities.semanticTokensProvider = nil
-          end
+					-- ts_ls の semanticTokens を無効化（重い）
+					if client and client.name == "ts_ls" then
+						client.server_capabilities.semanticTokensProvider = nil
+					end
 
-          local opts = { buffer = bufnr }
-          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-          vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
-          vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
-          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-        end,
-      })
+					local opts = { buffer = bufnr }
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
+					vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+				end,
+			})
 
-      -- ===== HTML =====
-      vim.lsp.config("html", {
-        capabilities = capabilities,
-      })
+			-- ===== HTML =====
+			vim.lsp.config("html", {
+				capabilities = capabilities,
+			})
 
-      -- ===== Lua =====
-      vim.lsp.config("lua_ls", {
-        capabilities = capabilities,
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = { "vim" },
-            },
-          },
-        },
-      })
+			-- ===== Lua =====
+			vim.lsp.config("lua_ls", {
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+					},
+				},
+			})
 
-      -- ===== Ruby =====
-      vim.lsp.config("solargraph", {
-        capabilities = capabilities,
-      })
+			-- ===== Ruby =====
+			vim.lsp.config("solargraph", {
+				capabilities = capabilities,
+			})
 
-      -- ===== JavaScript / TypeScript =====
-      vim.lsp.config("ts_ls", {
-        capabilities = capabilities,
-        cmd = { "typescript-language-server", "--stdio" },
-        filetypes = {
-          "javascript",
-          "javascriptreact",
-          "typescript",
-          "typescriptreact",
-        },
-        root_dir = vim.fs.root(0, {
-          "package.json",
-          "tsconfig.json",
-          ".git",
-        }),
-      })
+			-- ===== JavaScript / TypeScript =====
+			vim.lsp.config("ts_ls", {
+				capabilities = capabilities,
+				cmd = { "typescript-language-server", "--stdio" },
+				filetypes = {
+					"javascript",
+					"javascriptreact",
+					"typescript",
+					"typescriptreact",
+				},
+				root_dir = vim.fs.root(0, {
+					"package.json",
+					"tsconfig.json",
+					".git",
+				}),
+			})
 
-      vim.lsp.config("nixd", {
-        capabilities = capabilities,
-        cmd = { "nixfmt" },
-      })
+			vim.lsp.config("nixd", {
+				capabilities = capabilities,
+				cmd = { "nixfmt" },
+			})
 
-      -- ===== 有効化 =====
-      vim.lsp.enable({
-        "html",
-        "lua_ls",
-        "solargraph",
-        "ts_ls",
-        "nixd",
-      })
-    end,
-  },
+			vim.lsp.config("efm", {
+				capabilities = capabilities,
+			})
+
+			-- ===== 有効化 =====
+			vim.lsp.enable({
+				"html",
+				"lua_ls",
+				"solargraph",
+				"efm",
+				"ts_ls",
+				"nixd",
+			})
+		end,
+	},
 }
