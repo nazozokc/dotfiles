570318fb03c13b8bbdc5f9ff37135b538da6fa80
nazozokc <nazozokc@gmail.com>
2026-02-26 17:34:18 +0900

    edit yazi.lua

--- nvim/lua/plugins/yazi.lua
+++ nvim/lua/plugins/yazi.lua
@@ -1,23 +1,53 @@
 ---@type LazySpec
 return {
 	"mikavilpas/yazi.nvim",
-	version = "*", -- use the latest stable version
-	cmd = { "yazi" },
+	version = "*",
+	cmd = { "Yazi", "Yazi cwd", "Yazi toggle" },
 	dependencies = {
 		{ "nvim-lua/plenary.nvim", lazy = true },
 	},
 	opts = {
-		-- if you want to open yazi instead of netrw, see below for more info
 		open_for_directories = false,
+		open_multiple_tabs = true,
+		change_neovim_cwd_on_close = false,
+		floating_window_scaling_factor = 0.9,
+		yazi_floating_window_winblend = 0,
+		yazi_floating_window_border = "rounded",
+		highlight_hovered_buffers_in_same_directory = true,
 		keymaps = {
 			show_help = "<f1>",
+			open_file_in_vertical_split = "<c-v>",
+			open_file_in_horizontal_split = "<c-x>",
+			open_file_in_tab = "<c-t>",
+			grep_in_directory = "<c-s>",
+			replace_in_directory = "<c-g>",
+			cycle_open_buffers = "<tab>",
+			copy_relative_path_to_selected_files = "<c-y>",
+			send_to_quickfix_list = "<c-q>",
+			change_working_directory = "<c-\\>",
+			open_and_pick_window = "<c-o>",
+		},
+		integrations = {
+			grep_in_directory = function(directory)
+				local ok = pcall(require, "telescope")
+				if ok then
+					local telescope = require("telescope")
+					telescope.extensions.live_grep.live_grep({
+						cwd = directory,
+					})
+				end
+			end,
+			replace_in_directory = function(directory)
+				local ok = pcall(require, "grug-far")
+				if ok then
+					require("grug-far").open({
+						cwd = directory,
+					})
+				end
+			end,
 		},
 	},
-	-- 👇 if you use `open_for_directories=true`, this is recommended
 	init = function()
-		-- mark netrw as loaded so it's not loaded at all.
-		--
-		-- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
 		vim.g.loaded_netrwPlugin = 1
 	end,
 }
