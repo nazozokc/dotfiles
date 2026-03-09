11cc02f70a72be871786827910325410d57d9e18
nazozokc <nazozokc@gmail.com>
2026-03-09 19:17:05 +0900

    edit nvim

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -7,75 +7,65 @@ return {
 		{ "<leader>gH", "<cmd>DiffviewFileHistory<cr>", desc = "Diff: Branch History" },
 		{ "<leader>gc", "<cmd>DiffviewClose<cr>", desc = "Diff: Close" },
 	},
-	opts = {
-		enhanced_diff_hl = true, -- 変更箇所をより細かくハイライト
-		show_help_hints = false, -- ヒント表示はうるさいので消す
-		watch_index = true,
+	config = function()
+		local actions = require("diffview.actions")
 
-		-- diff表示はverticalの方が縦長モニターじゃなくても読みやすい
-		view = {
-			default = {
-				layout = "diff2_vertical",
-				disable_diagnostics = true, -- diff中はLSPエラー表示を切る
-			},
-			merge_tool = {
-				layout = "diff3_horizontal",
-				disable_diagnostics = true,
-				winbar_info = true,
-			},
-			file_history = {
-				layout = "diff2_vertical",
-				disable_diagnostics = true,
-			},
-		},
+		require("diffview").setup({
+			enhanced_diff_hl = true,
+			show_help_hints = false,
+			watch_index = true,
 
-		file_panel = {
-			listing_style = "tree",
-			tree_options = {
-				flatten_dirs = true,
-				folder_statuses = "only_folded",
-			},
-			win_config = {
-				position = "left",
-				width = 30, -- デフォルト35より少し狭く
-			},
-		},
-
-		file_history_panel = {
-			win_config = {
-				position = "bottom",
-				height = 12,
-			},
-		},
-
-		-- diff bufferのローカル設定をhookで上書き
-		hooks = {
-			diff_buf_read = function(bufnr)
-				vim.opt_local.wrap = false
-				vim.opt_local.relativenumber = false
-			end,
-		},
-
-		keymaps = {
-			disable_defaults = false,
 			view = {
-				-- デフォルトのtab/s-tabはそのまま使う
-				-- コンフリクト解消を直感的に
-				{ "n", "<leader>co", require("diffview.actions").conflict_choose("ours"), { desc = "Choose OURS" } },
-				{
-					"n",
-					"<leader>ct",
-					require("diffview.actions").conflict_choose("theirs"),
-					{ desc = "Choose THEIRS" },
+				default = {
+					layout = "diff2_vertical",
+					disable_diagnostics = true,
 				},
-				{ "n", "<leader>ca", require("diffview.actions").conflict_choose("all"), { desc = "Choose ALL" } },
-				{
-					"n",
-					"dx",
-					require("diffview.actions").conflict_choose("none"),
-					{ desc = "Delete conflict" },
+				merge_tool = {
+					layout = "diff3_horizontal",
+					disable_diagnostics = true,
+					winbar_info = true,
+				},
+				file_history = {
+					layout = "diff2_vertical",
+					disable_diagnostics = true,
 				},
 			},
-		},
-	},
+
+			file_panel = {
+				listing_style = "tree",
+				tree_options = {
+					flatten_dirs = true,
+					folder_statuses = "only_folded",
+				},
+				win_config = {
+					position = "left",
+					width = 30,
+				},
+			},
+
+			file_history_panel = {
+				win_config = {
+					position = "bottom",
+					height = 12,
+				},
+			},
+
+			hooks = {
+				diff_buf_read = function(bufnr)
+					vim.opt_local.wrap = false
+					vim.opt_local.relativenumber = false
+				end,
+			},
+
+			keymaps = {
+				disable_defaults = false,
+				view = {
+					{ "n", "<leader>co", actions.conflict_choose("ours"), { desc = "Choose OURS" } },
+					{ "n", "<leader>ct", actions.conflict_choose("theirs"), { desc = "Choose THEIRS" } },
+					{ "n", "<leader>ca", actions.conflict_choose("all"), { desc = "Choose ALL" } },
+					{ "n", "dx", actions.conflict_choose("none"), { desc = "Delete conflict" } },
+				},
+			},
+		})
+	end,
 }
