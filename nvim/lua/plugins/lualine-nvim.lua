64c8b4a045734ceb573f6c89635e05a06975f74c
nazozokc <nazozokc@gmail.com>
2026-03-09 16:09:22 +0900

    edit nvim

--- nvim/lua/plugins/lualine-nvim.lua
+++ nvim/lua/plugins/lualine-nvim.lua
@@ -1,6 +1,7 @@
 -- plugins/lualine.lua
 return {
 	"nvim-lualine/lualine.nvim",
+	event = "VeryLazy",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		require("lualine").setup({
