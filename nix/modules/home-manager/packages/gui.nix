a5e0f46c8849e8dd9682b8add07c1e986975fd64
nazozokc <nazozokc@gmail.com>
2026-03-12 15:43:33 +0900

    edit agents/skills

--- nix/modules/home-manager/packages/gui.nix
+++ nix/modules/home-manager/packages/gui.nix
@@ -1,11 +1,10 @@
+# nix/modules/home-manager/packages/gui.nix
 { pkgs }:
 
 with pkgs;
 [
-  # GUI software
   wezterm
   audacity
-  spotify
   discord
   google-chrome
   obsidian
@@ -13,4 +12,10 @@ with pkgs;
 ++ lib.optionals pkgs.stdenv.isLinux [
   ghostty
   tor-browser
+]
+++ lib.optionals (pkgs.stdenv.isLinux && pkgs.stdenv.hostPlatform.system == "x86_64-linux") [
+  spotify # x86_64-linux のみ
+]
+++ lib.optionals pkgs.stdenv.isDarwin [
+  spotify # aarch64-darwin は対応してる
 ]
