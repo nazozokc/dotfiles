e5dafc31f809a56eee2ce236d90e780868350527
nazozokc <nazozokc@gmail.com>
2026-03-16 17:05:18 +0900

    edit

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     ];
     extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g=" # ← これ
+      "cache.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g=" # ← これ
     ];
   };
 
