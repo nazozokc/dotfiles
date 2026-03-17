7d1bcdc48e9c7b5c3f4b1fcc585e93d5527d06a5
nazozokc <247927350+nazozokc@users.noreply.github.com>
2026-03-17 13:42:46 +0000

    style: auto-fix StyLua formatting

--- nvim/lua/plugins/nvim-ufo.lua
+++ nvim/lua/plugins/nvim-ufo.lua
@@ -1,20 +1,20 @@
 return {
- 	"kevinhwang91/nvim-ufo",
- 	event = "BufReadPost",
- 	dependencies = "kevinhwang91/promise-async",
- 	config = function()
- 		vim.o.foldcolumn = "1"
- 		vim.o.foldlevel = 99
- 		vim.o.foldlevelstart = 99
- 		vim.o.foldenable = true
+	"kevinhwang91/nvim-ufo",
+	event = "BufReadPost",
+	dependencies = "kevinhwang91/promise-async",
+	config = function()
+		vim.o.foldcolumn = "1"
+		vim.o.foldlevel = 99
+		vim.o.foldlevelstart = 99
+		vim.o.foldenable = true
 
- 		require("ufo").setup({
- 			provider_selector = function(_, _, _)
- 				return { "treesitter", "indent" }
- 			end,
- 		})
+		require("ufo").setup({
+			provider_selector = function(_, _, _)
+				return { "treesitter", "indent" }
+			end,
+		})
 
- 		vim.keymap.set("n", "zR", require("ufo").openAllFolds, { desc = "Open all folds" })
- 		vim.keymap.set("n", "zM", require("ufo").closeAllFolds, { desc = "Close all folds" })
- 	end,
- }
+		vim.keymap.set("n", "zR", require("ufo").openAllFolds, { desc = "Open all folds" })
+		vim.keymap.set("n", "zM", require("ufo").closeAllFolds, { desc = "Close all folds" })
+	end,
+}
