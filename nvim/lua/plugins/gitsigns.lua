c2bdb33403a48449c6b6a1ca0f4393585555b559
nazozokc <nazozokc@gmail.com>
2026-03-17 22:17:07 +0900

    fix(gitsigns): apply opts in config function

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,26 +1,27 @@
 return {
-	"lewis6991/gitsigns.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	opts = {
-		signs = {
-			add = { text = "▎" },
-			change = { text = "▎" },
-			delete = { text = "" },
-			topdelete = { text = "" },
-			changedelete = { text = "▎" },
-		},
+ 	"lewis6991/gitsigns.nvim",
+ 	event = { "BufReadPre", "BufNewFile" },
+ 	config = function()
+ 		require("gitsigns").setup({
+ 			signs = {
+ 				add = { text = "▎" },
+ 				change = { text = "▎" },
+ 				delete = { text = "" },
+ 				topdelete = { text = "" },
+ 				changedelete = { text = "▎" },
+ 			},
 
-		signcolumn = "yes:2",
-		watch_gitdir = {
-			follow_files = true,
-		},
+ 			signcolumn = "yes:2",
+ 			watch_gitdir = {
+ 				follow_files = true,
+ 			},
 
-		current_line_blame = false, -- ← 後で有効にする
-		word_diff = true, -- 🔥 単語単位差分
-		update_debounce = 100,
-	},
-	config = function()
-		vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-		vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
-	end,
-}
+ 			current_line_blame = false, -- ← 後で有効にする
+ 			word_diff = true, -- 🔥 単語単位差分
+ 			update_debounce = 100,
+ 		})
+
+ 		vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+ 		vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+ 	end,
+ }
