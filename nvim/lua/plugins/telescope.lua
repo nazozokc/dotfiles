108eb5bd02a11c859a801a2b725682c1b2cc0538
nazozokc <nazozokc@gmail.com>
2026-03-17 22:22:14 +0900

    fix(keymaps): resolve leader-f conflict between telescope and conform

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -19,7 +19,7 @@ return {
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-			vim.keymap.set("n", "<leader>f", builtin.oldfiles, {})
+			vim.keymap.set("n", "<leader>fo", builtin.oldfiles, {})
 
 			require("telescope").load_extension("ui-select")
 		end,
