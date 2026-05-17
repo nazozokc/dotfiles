5ada60335cb5e63dce1d797bba65edb532c79377
nazozokc <nazozokc@gmail.com>
2026-05-17 23:40:08 +0900

    edit

--- nix/modules/home-manager/dotfiles-link.nix
+++ nix/modules/home-manager/dotfiles-link.nix
@@ -8,7 +8,6 @@ in
     "commitlint.config.js".source = link "${dotfilesDir}/commitlint.config.js";
     ".config/fish".source = link "${dotfilesDir}/fish";
     ".config/wezterm".source = link "${dotfilesDir}/wezterm";
-    ".config/ghostty".source = link "${dotfilesDir}/ghostty";
     ".zshrc".source = link "${dotfilesDir}/zsh/zshrc";
     ".bashrc".source = link "${dotfilesDir}/bash/bashrc";
     ".scripts".source = link "${dotfilesDir}/my_scripts";
