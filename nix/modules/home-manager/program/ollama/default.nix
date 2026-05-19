35b48e8816292f4ad1fadfee8cf40830ee787ba6
nazozokc <nazozokc@gmail.com>
2026-05-19 21:15:24 +0900

    feat: enhance ollama config with service, oterm, and fish abbrs

--- nix/modules/home-manager/program/ollama/default.nix
+++ nix/modules/home-manager/program/ollama/default.nix
@@ -1,7 +1,18 @@
-{ pkgs, ... }:
-
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  services.ollama = {
+    enable = true;
+    # acceleration = null;  # null: デフォルト, "cuda": NVIDIA, "rocm": AMD
+    host = "127.0.0.1";
+    port = 11434;
+  };
+
   home.packages = with pkgs; [
-    ollama
+    oterm
   ];
 }
