e76aa24c431b54fd9613650f1db58e9ae8b7ec15
nazozokc <nazozokc@gmail.com>
2026-05-25 22:47:45 +0900

    fix: exclude fish_variables from nix symlink to avoid read-only store error

--- nix/modules/home/dotfiles-link.nix
+++ nix/modules/home/dotfiles-link.nix
@@ -5,7 +5,13 @@ let
 in
 {
   home.file = {
-    ".config/fish".source = link "${dotfilesDir}/fish";
+    # NOTE: fish ディレクトリ全体を symlink すると Nix store が read-only のため
+    # fish_variables の書き込みが失敗する。個別に symlink して fish_variables だけ
+    # Nix 管理から外し、fish に自動生成させる。
+    ".config/fish/fish_plugins".source = link "${dotfilesDir}/fish/fish_plugins";
+    ".config/fish/conf.d".source = link "${dotfilesDir}/fish/conf.d";
+    ".config/fish/functions".source = link "${dotfilesDir}/fish/functions";
+    ".config/fish/completions".source = link "${dotfilesDir}/fish/completions";
     ".config/wezterm".source = link "${dotfilesDir}/wezterm";
     ".zshrc".source = link "${dotfilesDir}/zsh/zshrc";
     ".bashrc".source = link "${dotfilesDir}/bash/bashrc";
