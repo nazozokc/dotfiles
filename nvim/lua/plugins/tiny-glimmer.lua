047c128f0f39ae00f0253acd9c7e62b0c1268db3
nazozokc <nazozokc@gmail.com>
2026-05-25 17:30:54 +0900

    edit

--- nvim/lua/plugins/tiny-glimmer.lua
+++ nvim/lua/plugins/tiny-glimmer.lua
@@ -1,7 +1,6 @@
 return {
 	"rachartier/tiny-glimmer.nvim",
 	event = "VeryLazy",
-	priority = 10, -- Low priority to catch other plugins' keybindings
 	config = function()
 		require("tiny-glimmer").setup({
 			enabled = true,
