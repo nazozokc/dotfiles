37fef40bd9ef16d02fe589757b91b14547d787ad
nazozokc <nazozokc@gmail.com>
2026-05-26 17:41:22 +0900

    edit

--- nvim/lua/plugins/nvim-cmp-cmdline.lua
+++ nvim/lua/plugins/nvim-cmp-cmdline.lua
@@ -2,6 +2,9 @@ return {
 	"hrsh7th/cmp-cmdline",
 	event = "CmdlineEnter",
 	-- dependencies に nvim-cmp を書かない
+	--- PATCH: vim.fn.getcompletion() が nil を返すケースへの対応
+	--- Neovim 0.12 の vim.validate が nil を許容しないため
+	--- 該当箇所: cmp_cmdline/init.lua:136 に nil ガードを追加
 	config = function()
 		local ok, cmp = pcall(require, "cmp")
 		if not ok then
