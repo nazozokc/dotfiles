29f85c79b000489529e1f45d6a575c278260ba98
nazozokc <nazozokc@gmail.com>
2026-05-31 11:01:03 +0900

    edit

--- nix/shared.nix
+++ nix/shared.nix
@@ -6,7 +6,9 @@
   home.homeDirectory = if pkgs.stdenv.isDarwin then "/Users/${username}" else "/home/${username}";
 
   # Keep in sync with your nixpkgs channel version
-  home.stateVersion = "26.05";
+  home.stateVersion = "26.11";
+
+  home.enableNixpkgsReleaseCheck = false;
 
   xdg.enable = true;
 
