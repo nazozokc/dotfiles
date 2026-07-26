27497016f0428ef53afd48abc0cd3c420ad9ed8d
nazozokc <nazozokc@users.noreply.github.com>
2026-07-26 17:47:51 +0900

    edit

--- nix/modules/home/programs/gtk.nix
+++ nix/modules/home/programs/gtk.nix
@@ -26,9 +26,9 @@
   };
 
   # Force overwrite existing files managed by home-manager's gtk module
+  gtk.gtk2.force = true;
   xdg.configFile."gtk-3.0/settings.ini".force = true;
   xdg.configFile."gtk-4.0/settings.ini".force = true;
-  home.file.".gtkrc-2.0".force = true;
 
   # ===== Qt theme (follow GTK) =====
   qt = {
