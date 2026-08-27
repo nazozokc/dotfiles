98e97dd07ec764f00fca6751dd140006946132ec
nazozokc <nazozokc@users.noreply.github.com>
2026-08-27 17:54:59 +0900

    edit

--- nvim/lua/plugins/wordcount.lua
+++ nvim/lua/plugins/wordcount.lua
@@ -6,6 +6,9 @@ return {
 			position = "bottom_right", -- top_left / top_right / bottom_left / bottom_right
 			debounce_ms = 100,
 			format = "Characters: %d",
+			markdown = {
+				include_frontmatter = true,
+			},
 		})
 	end,
 }
