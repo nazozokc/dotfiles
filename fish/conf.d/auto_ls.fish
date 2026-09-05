2d5f7cba1818a617440115934ba674c68445cd37
nazozokc <nazozokc@users.noreply.github.com>
2026-09-05 12:06:39 +0900

    edit

--- fish/conf.d/auto_ls.fish
+++ fish/conf.d/auto_ls.fish
@@ -1,4 +1,7 @@
 # cd したら自動で ls
+# NOTE: イベントハンドラは autoload では登録されないため conf.d に置く
+# (fish docs: Event handlers only become active once the corresponding
+#  function is loaded into the shell)
 function __auto_ls --on-variable PWD
     # 端末幅が狭いと邪魔なので制限
     if test (tput cols) -ge 60
