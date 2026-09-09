60f7b0cc54e68f1ef1822d1d2839e03cb5fbc386
nazozokc <nazozokc@users.noreply.github.com>
2026-09-09 22:00:49 +0900

    edit

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -47,5 +47,5 @@ if ime_off_cmd then
 	})
 end
 
-vim.keymap.set("", "<F5>", "<Nop>", { noremap = true, silent = true })
-vim.keymap.set("", "<F6>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("", "<80>F5", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("", "<80>F6", "<Nop>", { noremap = true, silent = true })
