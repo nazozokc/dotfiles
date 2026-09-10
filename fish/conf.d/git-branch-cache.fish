a524f21d5623a4e35811019214b4da6026008f5d
nazozokc <nazozokc@users.noreply.github.com>
2026-09-10 23:46:27 +0900

    edit

--- fish/conf.d/git-branch-cache.fish
+++ fish/conf.d/git-branch-cache.fish
@@ -19,9 +19,10 @@ function __update_git_branch_on_pwd --on-variable PWD
 end
 
 # 全コマンド実行後（git checkout / switch 等のキャッチのため）
-function __update_git_branch_on_postexec --event fish_postexec
+function __update_git_branch_on_postexec
     __update_git_branch
 end
+event fish_postexec __update_git_branch_on_postexec
 
 # 起動時の初期化
 __update_git_branch
