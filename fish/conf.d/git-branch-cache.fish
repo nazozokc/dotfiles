96e59efec135bce7f5fe085f41174ac7fb2bde52
nazozokc <nazozokc@users.noreply.github.com>
2026-09-10 23:34:20 +0900

    edit

--- fish/conf.d/git-branch-cache.fish
+++ fish/conf.d/git-branch-cache.fish
@@ -1,18 +1,26 @@
 # Git ブランチ名のキャッシュ
-# PWD 変更時にのみ git symbolic-ref を実行し、
-# git_branch() 関数がキャッシュ変数を返すだけで済むようにする。
+# PWD 変更時 + 全コマンド実行後に git symbolic-ref を実行し、
+# git checkout / git switch 等のディレクトリ移動なしブランチ切替でも反映されるようにする。
 
 set -g __git_branch_cache ""
 
-function __update_git_branch --on-variable PWD
-    # gitリポジトリのルートを取得してからブランチを取得
+function __update_git_branch
     set -l root (command git rev-parse --show-toplevel 2>/dev/null)
     if test -n "$root"
-        # gitリポジトリ内でgitコマンドを実行
         set -g __git_branch_cache (command git -C "$root" symbolic-ref --short HEAD 2>/dev/null)
     else
         set -g __git_branch_cache ""
     end
+end
+
+# PWD 変更時
+function __update_git_branch_on_pwd --on-variable PWD
+    __update_git_branch
+end
+
+# 全コマンド実行後（git checkout / switch 等のキャッチのため）
+function __update_git_branch_on_postexec --event fish_postexec
+    __update_git_branch
 end
 
 # 起動時の初期化
